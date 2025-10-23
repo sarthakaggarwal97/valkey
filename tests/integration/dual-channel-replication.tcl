d7baa2b3e6ba4ff868aae46a453a53037312b931
Sarthak Aggarwal <sarthagg@amazon.com>
2025-10-23 10:33:46 -0700

    does pushing work #4?
    Signed-off-by: Sarthak Aggarwal <sarthagg@amazon.com>

--- tests/integration/dual-channel-replication.tcl
+++ tests/integration/dual-channel-replication.tcl
@@ -48,7 +48,7 @@ start_server {tags {"dual-channel-replication external:skip"}} {
         }
 
         # Avoids timeout by keeping the RDB child alive longer while the replica is inactive
-        $primary config set rdb-key-save-delay 1000
+        $primary config set rdb-key-save-delay 200
         populate 10000 primary 10000
         
         set load_handle1 [start_one_key_write_load $primary_host $primary_port 100 "mykey1"]
@@ -71,7 +71,6 @@ start_server {tags {"dual-channel-replication external:skip"}} {
                 fail "Primary should allow backlog (have=$cur, need>[expr {2 * $backlog_size}]) to grow beyond its limits during dual-channel-replication sync handshake"
             }
             wait_and_resume_process -1
-            $replica debug pause-after-fork 0
 
             verify_replica_online $primary 0 500
             wait_for_condition 50 1000 {
