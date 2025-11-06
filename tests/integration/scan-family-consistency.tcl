92f98399a2b5e1e954d52feb633b0559ba0d6aa0
Sarthak Aggarwal <sarthagg@amazon.com>
2025-11-06 09:26:43 -0800

    let's see if it works #14
    Signed-off-by: Sarthak Aggarwal <sarthagg@amazon.com>

--- tests/integration/scan-family-consistency.tcl
+++ tests/integration/scan-family-consistency.tcl
@@ -42,7 +42,7 @@ test {scan family consistency with configured hash seed} {
                 $primary config set hz 0
                 $replica config set hz 0
 
-                after 100
+                after 1000
 
                 set cursor {{0} {}}
                 while {1} {
