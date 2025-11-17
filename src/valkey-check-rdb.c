2473b6b3f52a35a88b65d25ad4490470ac27bb7c
Sarthak Aggarwal <sarthagg@amazon.com>
2025-11-17 08:11:06 -0800

    fix

--- src/valkey-check-rdb.c
+++ src/valkey-check-rdb.c
@@ -656,8 +656,6 @@ int redis_check_rdb(char *rdbfilename, FILE *fp) {
         r->update_cksum = rdbLoadProgressCallback;
         /* Seed the chunk rio checksum with the header bytes we already read */
         r->cksum = rdb.cksum;
-        /* Update rdbstate.rio for error reporting */
-        rdbstate.rio = r;
         /* Clear the callback on the underlying rio to avoid double counting progress.
          * The chunk rio will handle progress reporting on decompressed bytes. */
         rdb.update_cksum = NULL;
