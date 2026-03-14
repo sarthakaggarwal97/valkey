83861261acec95fc8e37b89c5979c71b9f647927
Sarthak Aggarwal <sarthakaggarwal97@gmail.com>
2026-03-13 17:57:24 -0700

    Merge pull request #91 from sarthakaggarwal97/test-conflict-backport
    Add comment near version define in version.h

--- src/version.h
+++ src/version.h
@@ -4,8 +4,19 @@
  * similar. */
 #define SERVER_NAME "valkey"
 #define SERVER_TITLE "Valkey"
+<<<<<<< HEAD
 #define VALKEY_VERSION "8.0.7"
 #define VALKEY_VERSION_NUM 0x00080007
+=======
+#define VALKEY_VERSION "255.255.255"
+/* Backport conflict test: this comment will cause a merge conflict */
+#define VALKEY_VERSION_NUM 0x00ffffff
+/* The release stage is used in order to provide release status information.
+ * In unstable branch the status is always "dev".
+ * During release process the status will be set to rc1,rc2...rcN.
+ * When the version is released the status will be "ga". */
+#define VALKEY_RELEASE_STAGE "dev"
+>>>>>>> c01daf80c (Merge pull request #91 from sarthakaggarwal97/test-conflict-backport)
 
 /* Redis OSS compatibility version, should never
  * exceed 7.2.x. */
