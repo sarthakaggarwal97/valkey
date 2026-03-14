c04221b771e7131431546238462f0dc5c42fbfd5
Sarthak Aggarwal <sarthakaggarwal97@gmail.com>
2026-03-13 21:21:29 -0700

    Merge pull request #99 from sarthakaggarwal97/test-conflict-v5
    Add release stage comment to version.h

--- src/version.h
+++ src/version.h
@@ -7,6 +7,12 @@
 #define VALKEY_VERSION "8.0.7"
 #define VALKEY_VERSION_NUM 0x00080007
 
+/* The release stage is used in order to provide release status information.
+ * In unstable branch the status is always "dev".
+ * During release process the status will be set to rc1,rc2...rcN.
+ * When the version is released the status will be "ga". */
+#define VALKEY_RELEASE_STAGE "dev" /* auto-set during release */
+
 /* Redis OSS compatibility version, should never
  * exceed 7.2.x. */
 #define REDIS_VERSION "7.2.4"
