e825b268871d99a101f73bc0f96f08fbbc1837c0
Sarthak Aggarwal <sarthakaggarwal97@gmail.com>
2026-03-13 21:21:15 -0700

    Add release stage comment to version.h

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * In unstable branch the status is always "dev".
  * During release process the status will be set to rc1,rc2...rcN.
  * When the version is released the status will be "ga". */
-#define VALKEY_RELEASE_STAGE "dev"
+#define VALKEY_RELEASE_STAGE "dev" /* auto-set during release */
 
 /* Redis OSS compatibility version, should never
  * exceed 7.2.x. */
