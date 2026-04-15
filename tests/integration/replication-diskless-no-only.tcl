3da8326b238e643124fc3b8f57aab11519df1816
Sarthak Aggarwal <25262500+sarthakaggarwal97@users.noreply.github.com>
2026-04-14 18:56:56 -0700

    Raise focused diskless replication wait to 180s

--- tests/integration/replication-diskless-no-only.tcl
+++ tests/integration/replication-diskless-no-only.tcl
@@ -39,7 +39,7 @@ start_server {tags {"repl external:skip"} overrides {save ""}} {
                 after 1500
                 resume_process $slow_replica_pid
 
-                wait_for_condition 1500 100 {
+                wait_for_condition 1800 100 {
                     [s -2 rdb_bgsave_in_progress] == 0
                 } else {
                     fail "rdb child didn't terminate"
