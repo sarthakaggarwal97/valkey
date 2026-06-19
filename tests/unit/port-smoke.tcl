d19bea1d57b1fafc76ddfa77b3fa49c996fdf492
Sarthak Aggarwal <sarthagg@amazon.com>
2026-06-19 03:57:13 +0000

    Fix port-smoke test to expect fixed value (#9999)
    Signed-off-by: Sarthak Aggarwal <sarthagg@amazon.com>
    (cherry picked from commit c49b33e594a888628197754d127e6da3bfe849af)

--- tests/unit/port-smoke.tcl
+++ tests/unit/port-smoke.tcl
@@ -1,6 +1,6 @@
 start_server {tags {"port-smoke"}} {
     test {port-smoke: fixed behavior returns OK} {
-        r set port-smoke-key broken
+        r set port-smoke-key fixed
         assert_equal [r get port-smoke-key] "fixed"
     }
 }
